ef7107f2c2b8903b587f68b4117da4e21b913d3b
lilkaPTS <61536273+lilkaPTS@users.noreply.github.com>
2022-01-23 20:42:41 +0300

    removal of not need

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {HttpClient, HttpEvent, HttpRequest, HttpResponse} from "@angular/common/http";
 import {Observable, Subscription} from "rxjs";
 import {JsonPipe} from "@angular/common";
@@ -16,8 +16,6 @@ export class AppComponent {
 
   file1: File| null = null;
   file2: File| null = null;
-  body1: any;
-  body2: any;
 
   json1: string = "";
   json2: string = "";
@@ -34,42 +32,14 @@ export class AppComponent {
   }
 
   click(): void {
-    // console.log(this.file1);
-    // console.log(this.file2);
-
-
-    // this.upload().subscribe(
-    //   (event: any) => {
-    //     if (event instanceof HttpResponse) {
-    //       this.body1 = event.body[0];
-    //       this.body2 = event.body[1];
-    //       console.log(this.body1);
-    //     }
-    //   },
-    //   (err: any) => {
-    //     alert(err);
-    //   });
-    this.upload1().subscribe((data:any) => {
+    this.upload().subscribe((data:any) => {
       this.json1 = data[0];
       this.json2 = data[1];
     });
 
   }
 
-  upload(): Observable<HttpEvent<any>> {
-    const formData: FormData = new FormData();
-    if(this.file1 != null && this.file2 != null) {
-      formData.append('file1', this.file1);
-      formData.append('file2', this.file2);
-    }
-    const req = new HttpRequest('POST', `${this.baseUrl}/test`, formData, {
-      reportProgress: true,
-      responseType: 'json'
-    });
-    return this.http.request(req);
-  }
-
-  upload1(){
+  upload(){
     const formData: FormData = new FormData();
     if(this.file1 != null && this.file2 != null) {
       formData.append('file1', this.file1);
